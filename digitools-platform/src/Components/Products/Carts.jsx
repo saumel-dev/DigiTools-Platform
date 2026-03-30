b48f2b8b58171c05af3e7d84e3b9248cdf21e4b9
Saumel-Das <saumel.spr@gmail.com>
2026-03-30 17:47:36 +0600

    rating section created

--- digitools-platform/src/Components/Products/Carts.jsx
+++ digitools-platform/src/Components/Products/Carts.jsx
@@ -1,16 +1,34 @@
 import React, { use } from 'react';
-import writing from '../../assets/products/design-tool.png'
-const Carts = ({cart}) => {
-    const {name, description, price, period, tag, tagType, features, icon} = cart;
+import Prod_des from './Prod_des';
+const Carts = ({ cart }) => {
+    const { name, description, price, period, tag, tagType, features, icon } = cart;
+    const tagStyle = 
+    tagType === 'popular'? 'badge-primary badge-soft bg-linear-to-b from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent'
+    : tagType === 'new'? 'text-[#0A883E] bg-green-100'
+    : tagType === 'best-seller'? 'text-[#BB4D00] bg-yellow-100': '';
     return (
         <>
-           <div>
-            <div className='card w-96 flex bg-gray-400 shadow-sm'>
-                <div className='bg-blue-300 badge m-2 justify-end'>
-                    <p>{tag}</p>
+            <div className="card w-96 bg-base-100 shadow-md">
+                <div className="card-body relative">
+                    <span className={`badge ${tagStyle} badge-xs text-[14px] px-3 py-2 absolute right-5`}>{tag}</span>
+                    <div className="mt-6 border border-base-300 rounded-full flex items-center justify-center w-14 h-14">
+                        <img className='object-contain' src={icon} alt="" />
+                    </div>
+                    <div className="mt-2 space-y-4">
+                        <h2 className="text-2xl font-bold">{name}</h2>
+                        <p className='text-[#627382]'>{description}</p>
+                    </div>
+                        <h3> <span className='text-2xl font-semibold'>${price}</span> <span className='text-[#627382]'>/{period}</span></h3>
+                    <div>
+                    </div>
+                    {
+                        features.map((feature, index) => <Prod_des key={index} feature={feature}></Prod_des>)
+                    }
+                    <div className="">
+                        <button className="btn bg-linear-to-b from-[#4F39F6] to-[#9514FA] text-white rounded-full btn-block">Buy Now</button>
+                    </div>
                 </div>
             </div>
-           </div>
         </>
     );
 };
---
+++ digitools-platform/src/Components/Products/Prod_des.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Prod_des = ({ feature }) => {
+    console.log(feature);
+
+    return (
+        <>
+            <ul className="flex flex-col gap-2 text-[#627382]">
+                <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                    <span>{feature}</span>
+                </li>
+            </ul>
+        </>
+    );
+};
+
+export default Prod_des;
